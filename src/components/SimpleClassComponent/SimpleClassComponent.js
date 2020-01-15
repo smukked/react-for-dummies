9f565e10ee19dec471468755de0c0285f1ced784
Danny Schønning <dannys@proactive.dk>
2020-01-15 20:15:20 +0100

    Update simple

--- src/components/SimpleClassComponent/SimpleClassComponent.js
+++ src/components/SimpleClassComponent/SimpleClassComponent.js
@@ -4,15 +4,22 @@ export default class SimpleClassComponent extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            loaded: "I am loading"
+        }
     }
 
     componentDidMount() {
-        
+        window.setTimeout(() => {
+            this.setState({
+                loaded: "I am loaded!"
+            })
+        }, 1000)
     }
 
     render() {
         return (
-            <div>My Simple Class Component</div>
+            <div>{this.state.loaded}</div>
         )
     }
 }
